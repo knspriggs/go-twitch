2f577ac2b35a6f4c2403c5f621a0890406f25af3
Kristian Spriggs <kristianspriggs@icloud.com>
2016-06-30 17:50:30 -0400

    Fix formatting

--- constants.go
+++ constants.go
@@ -1,6 +1,6 @@
 package twitch
 
 const (
-  DefaultURL = "https://api.twitch.tv/kraken"
-  APIV3Header = "application/vnd.twitchtv.v3+json"
+	DefaultURL  = "https://api.twitch.tv/kraken"
+	APIV3Header = "application/vnd.twitchtv.v3+json"
 )
--- streams_test.go
+++ streams_test.go
@@ -1,88 +1,88 @@
 package twitch
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetStreamByChannel(t *testing.T) {
-  req := &GetStreamByChannelRequestType{
-    Channel: "#knspriggs",
-  }
-  session, err := NewSession(DefaultURL, APIV3Header)
-  resp, err := session.GetStreamByChannel(req)
-  assert.Nil(t, err)
-  if assert.NotNil(t, resp) {
-    assert.Equal(t, resp.Stream.Viewers, 0)
-  }
+	req := &GetStreamByChannelRequestType{
+		Channel: "#knspriggs",
+	}
+	session, err := NewSession(DefaultURL, APIV3Header)
+	resp, err := session.GetStreamByChannel(req)
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.Equal(t, resp.Stream.Viewers, 0)
+	}
 }
 
 func TestGetStreamsWithoutRequestParams(t *testing.T) {
-  req := &GetStreamsRequestType{}
-  session, err := NewSession(DefaultURL, APIV3Header)
-  resp, err := session.GetStream(req)
-  assert.Nil(t, err)
-  if assert.NotNil(t, resp) {
-    assert.NotEqual(t, resp.Streams, 0)
-  }
+	req := &GetStreamsRequestType{}
+	session, err := NewSession(DefaultURL, APIV3Header)
+	resp, err := session.GetStream(req)
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.NotEqual(t, resp.Streams, 0)
+	}
 }
 
 func TestGetStreamsWithPartialRequestParamsAndDefaults(t *testing.T) {
-  req := &GetStreamsRequestType{
-    Game: "Counter-Strike: Global Offensive",
-  }
-  session, err := NewSession(DefaultURL, APIV3Header)
-  resp, err := session.GetStream(req)
-  assert.Nil(t, err)
-  if assert.NotNil(t, resp) {
-    assert.Equal(t, len(resp.Streams), 25)
-  }
+	req := &GetStreamsRequestType{
+		Game: "Counter-Strike: Global Offensive",
+	}
+	session, err := NewSession(DefaultURL, APIV3Header)
+	resp, err := session.GetStream(req)
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.Equal(t, len(resp.Streams), 25)
+	}
 }
 
 func TestGetStreamsWithPartialRequestParams(t *testing.T) {
-  req := &GetStreamsRequestType{
-    Game: "Counter-Strike: Global Offensive",
-    Limit: 10,
-    Offset: 1,
-    StreamType: "live",
-  }
-  session, err := NewSession(DefaultURL, APIV3Header)
-  resp, err := session.GetStream(req)
-  assert.Nil(t, err)
-  if assert.NotNil(t, resp) {
-    assert.Equal(t, len(resp.Streams), 10)
-  }
+	req := &GetStreamsRequestType{
+		Game:       "Counter-Strike: Global Offensive",
+		Limit:      10,
+		Offset:     1,
+		StreamType: "live",
+	}
+	session, err := NewSession(DefaultURL, APIV3Header)
+	resp, err := session.GetStream(req)
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.Equal(t, len(resp.Streams), 10)
+	}
 }
 
 func TestGetFeaturedStreams(t *testing.T) {
-  req := &GetFeaturedStreamsRequestType{}
-  session, err := NewSession(DefaultURL, APIV3Header)
-  resp, err := session.GetFeaturedStreams(req)
-  assert.Nil(t, err)
-  if assert.NotNil(t, resp) {
-    assert.Equal(t, len(resp.Featured), 25)
-  }
+	req := &GetFeaturedStreamsRequestType{}
+	session, err := NewSession(DefaultURL, APIV3Header)
+	resp, err := session.GetFeaturedStreams(req)
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.Equal(t, len(resp.Featured), 25)
+	}
 }
 
 func TestGetStreamsSummaryWithGame(t *testing.T) {
-  req := &GetStreamsSummaryRequestType{
-    Game: "Counter-Strike: Global Offensive",
-  }
-  session, err := NewSession(DefaultURL, APIV3Header)
-  resp, err := session.GetStreamsSummary(req)
-  assert.Nil(t, err)
-  if assert.NotNil(t, resp) {
-    assert.NotEqual(t, resp.Viewers, 0)
-  }
+	req := &GetStreamsSummaryRequestType{
+		Game: "Counter-Strike: Global Offensive",
+	}
+	session, err := NewSession(DefaultURL, APIV3Header)
+	resp, err := session.GetStreamsSummary(req)
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.NotEqual(t, resp.Viewers, 0)
+	}
 }
 
 func TestGetStreamsSummaryWithoutGame(t *testing.T) {
-  req := &GetStreamsSummaryRequestType{}
-  session, err := NewSession(DefaultURL, APIV3Header)
-  resp, err := session.GetStreamsSummary(req)
-  assert.Nil(t, err)
-  if assert.NotNil(t, resp) {
-    assert.NotEqual(t, resp.Viewers, 0)
-  }
+	req := &GetStreamsSummaryRequestType{}
+	session, err := NewSession(DefaultURL, APIV3Header)
+	resp, err := session.GetStreamsSummary(req)
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		assert.NotEqual(t, resp.Viewers, 0)
+	}
 }
